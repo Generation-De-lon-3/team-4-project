4bf3e7d5f2199b98bef3cea588e7ef2f2387c1f2
Dirie Isse <dirieisse0@mail.com>
2021-08-16 14:41:46 +0100

    adding extract

--- extract.py
+++ extract.py
@@ -2,8 +2,7 @@ import csv
 
 cafe_data = []
 
-path = r'C:\Users\lovef\Desktop\generation\Group_project\team-4-project\isle_of_wight.csv'
-with open (path, 'r') as file:
+with open ('isle_of_wight', 'r') as file:
     reader = csv.DictReader(file)
     for line in reader:
         cafe_data.append(line)
