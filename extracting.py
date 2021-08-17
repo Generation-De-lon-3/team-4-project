ca19eccfb629b68ecbb53ee4f4bc53f047bbb380
Dirie Isse <dirieisse0@mail.com>
2021-08-17 14:08:28 +0100

    extracts the csv data and validates empty or none containing values

--- extracting.py
+++ extracting.py
@@ -1,10 +1,20 @@
 import csv 
 
 cafe_data = []
+try:
+    with open ('2021-02-23-isle-of-wight.csv', 'r') as file:
+        reader = csv.DictReader(file, fieldnames=['timestamp_of_purhcase','store_name','customer_name','basket_items','payment_method','total_price','card_number'])
+        next(reader,None)
+        i=0
+        for line in reader:
+            for value in line.values():
+                if value == "" or value == "None":
+                    i+=1
+                    print(line)
+                    print(i)  
+            # cafe_data.append(line)
+except Exception as e:
+    print(e)
 
-with open ('isle_of_wight', 'r') as file:
-    reader = csv.DictReader(file)
-    for line in reader:
-        cafe_data.append(line)
+# print(cafe_data)
 
-print(cafe_data)
