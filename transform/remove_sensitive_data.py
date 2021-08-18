61cb654cb5a72261096c063b1b91cc7a9cba6859
Junaid <junaidgtd@gmail.com>
2021-08-18 10:42:26 +0100

    chenged filednames to match schema

--- transform/remove_sensitive_data.py
+++ transform/remove_sensitive_data.py
@@ -2,12 +2,11 @@ import pandas as pd
 import numpy as np
 
 cafe_file_path = '2021-02-23-isle-of-wight.csv'
-df = pd.read_csv(cafe_file_path, na_values='n/a', names=["timestamp of purchase", "location", "customer name", "basket items (name, size and price)", "cash or card payment", "total price", "card number (empty if cash)"])
+df = pd.read_csv(cafe_file_path, na_values='n/a', names=["timestamp", "branch", "customer", "basket", "payment_method", "total_price", "card"])
 
 
 del df['customer name'] 
 #Customer name has been removed
-
 
 
 col = 'card number (empty if cash)'
@@ -22,6 +21,6 @@ for i in range(len(df[col])):
 #sum of null cells is card info
 print(df[col].isnull().sum())
 
-
-print(df['card number (empty if cash)'].head())
+#print output
 print(df.head())
+
