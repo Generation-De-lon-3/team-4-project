e30417611913cf93b7c5e257c87a80deebba6993
Junaid <junaidgtd@gmail.com>
2021-08-20 12:22:54 +0100

    updated sql table schema

--- .devcontainer/db/init.sql
+++ .devcontainer/db/init.sql
@@ -10,21 +10,28 @@ CREATE TABLE IF NOT EXISTS products (
 PRIMARY KEY (product_id)
 );
 
+CREATE TABLE IF NOT EXISTS branch (
+	branch_id SERIAL NOT NULL,
+	branch varchar(22) NOT NULL,
+PRIMARY KEY (branch_id)
+);
+
+CREATE TABLE IF NOT EXISTS payments (
+	payment_id int NOT NULL,
+	payment_method varchar(10) NOT NULL,
+	card_type varchar(10),
+PRIMARY KEY (payment_id)
+);
+
 CREATE TABLE IF NOT EXISTS orders (
 	order_id SERIAL NOT NULL,
 	timestamp_of_purchase timestamp NOT NULL,
-	branch_place varchar(100) NOT NULL,
+	FOREIGN KEY (branch_id) REFERENCES branch (branch_id)
+	total_price float NOT NULL,
+	FOREIGN KEY (payment_id) REFERENCES payments (payment_id)
 PRIMARY KEY (order_id)
 );
 
-CREATE TABLE IF NOT EXISTS payment (
-	payment_id int NOT NULL,
-	payment_type varchar(10) NOT NULL,
-	card_type varchar(10),
-	payment_total float NOT NULL,
-	FOREIGN KEY (payment_id) REFERENCES orders (order_id),
-PRIMARY KEY (payment_id)
-);
 
 CREATE TABLE IF NOT EXISTS basket (
 	basket_id SERIAL NOT NULL,
