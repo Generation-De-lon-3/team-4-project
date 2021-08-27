fe4dd7d46ceef50319dea70bef9c61f8e8a4dd7c
stefan <generation@dichev.me>
2021-08-27 13:43:53 +0100

    new

--- .devcontainer/db/init.sql
+++ .devcontainer/db/init.sql
@@ -14,10 +14,7 @@ PRIMARY KEY (product_id)
 CREATE TABLE IF NOT EXISTS branches (
 	branch_id SERIAL NOT NULL,
 	branch_name varchar(22) NOT NULL,
-<<<<<<< HEAD
-=======
 	UNIQUE (branch_name),
->>>>>>> stefan
 PRIMARY KEY (branch_id)
 );
 
@@ -26,29 +23,11 @@ CREATE TABLE IF NOT EXISTS orders (
 	order_timestamp timestamp NOT NULL,
 	branch_id int NOT NULL,
 	FOREIGN KEY (branch_id) REFERENCES branches (branch_id),
-<<<<<<< HEAD
-PRIMARY KEY (order_id)
-);
-
-CREATE TABLE IF NOT EXISTS payments (
-	payment_id int NOT NULL,
-	payment_method varchar(10) NOT NULL,
-	card_type varchar(22),
-	total_payment float NOT NULL,
-	order_id int NOT NULL,
-	FOREIGN KEY (order_id) REFERENCES orders (order_id),
-PRIMARY KEY (payment_id)
-);
-
-CREATE TABLE IF NOT EXISTS baskets (
-	basket_id SERIAL NOT NULL,
-=======
 	UNIQUE (order_timestamp, branch_id),
 PRIMARY KEY (order_id)
 );
 
 CREATE TABLE IF NOT EXISTS baskets (
->>>>>>> stefan
 	order_id int NOT NULL,
 	product_id int NOT NULL,
 	product_quantity int NOT NULL,
--- src/app.py
+++ src/app.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import json
 
-<<<<<<< HEAD
-df = pd.read_csv('2021-02-23-isle-of-wight.csv', names=["timestamp", "branch", "customer", 'basket', "payment_method", "total_price", "card"])
-=======
 
 df = pd.read_csv('data/2021-02-23-isle-of-wight.csv', names=["order_timestamp", "branch", "customer", "basket", "payment_method", "payment_total", "card_type"])
->>>>>>> stefan
 
 del df["customer"] 
 
@@ -25,15 +21,9 @@ for item in cafe_dict:
 
     for index, items in enumerate(item['basket']):  
         item['basket'][index] = dict(zip(basket_fields,items))
-<<<<<<< HEAD
-        
-print(json.dumps(cafe_data, indent=4))
-#  [0]['basket'][0]['name'])
-=======
 
     item['card_type'] = "".join(i for i in item['card_type'] if i.isalpha())
     
     item["card_type"]= item["card_type"].replace("None", "")
     
     
->>>>>>> stefan
