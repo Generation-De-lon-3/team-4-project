91db8814a4f12574913fbde0632ee4b96484a249
stefan <generation@dichev.me>
2021-09-13 17:07:08 +0100

    :)

--- team4-sls-split/orders.py
+++ team4-sls-split/orders.py
@@ -9,13 +9,16 @@ def orders(data):
     cafe = pd.DataFrame(data)
     branches = pd.read_sql_query("SELECT * FROM branches", connection)
     orders = pd.read_sql_query("SELECT order_timestamp, branch_id FROM orders;", connection)
-    
     orders["order_timestamp"] = orders["order_timestamp"].astype(str)
     
     merged = pd.merge(branches, cafe[["order_timestamp", "branch_name"]], on="branch_name", how="right") 
-    merged = orders.merge(merged, on=["order_timestamp", "branch_id"], how="right", indicator=True)
+    
+    orders["x"] = orders.groupby(["order_timestamp", "branch_id"]).cumcount()
+    merged["x"] = merged.groupby("order_timestamp").cumcount()
+    
+    merged = orders.merge(merged, on=("order_timestamp", "branch_id", "x"), how="right", indicator=True) 
     ordervalues = merged[merged["_merge"] == "right_only"]
-                    
+    
     sql = "INSERT INTO orders (order_timestamp, branch_id) VALUES (%s, %s)"
 
     if not ordervalues.empty:
